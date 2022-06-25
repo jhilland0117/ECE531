7b0116cc8cf20d95a3295876cc6b318f2dda508d
jhilland <hillandjoseph@gmail.com>
2022-06-24 21:35:56 -0600

    working version but 501s on put, delete, post

--- code/hilland_hw2/assignment2.c
+++ code/hilland_hw2/assignment2.c
@@ -10,8 +10,10 @@
 
 const char *argp_program_version = "1.0.0.dev1";
 const char *argp_program_bug_address = "jhilland@unm.edu";
+struct curl_slist *headers = NULL;
 CURL *curl;
 
+// arguments will be used for storing values from command line
 struct Arguments {
     char *arg;  // for string argument
     char *url;    
@@ -21,6 +23,7 @@ struct Arguments {
     bool delete;
 };
 
+// argp options required for output to user
 static struct argp_option options[] = {
     {"url", 'u', "String", NO_ARG, "URL for HTTP Request"},
     {"post", 'o', NO_ARG, NO_ARG, "POST HTTP Request"},
@@ -31,20 +34,83 @@ static struct argp_option options[] = {
 };
 
 // http request methods, improve by making one method takes method type as param
-static void send_get_request(char *url, char *message) {
-    printf("sending get request at url: %s and message: %s\n", url, message);
+static int send_get_request(char *url) {
+    printf("sending get request at url: %s and message: %s\n", url);
+    if (curl) {
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK) {
+            return REQ_ERR;
+        }
+
+        curl_easy_cleanup(curl);
+    } else {
+        return INIT_ERR;
+    }
+    return OK;
 }
 
-static void send_post_request(char *url, char *message) {
+static int send_post_request(char *url, char *message) {
     printf("sending post request at url: %s and message: %s\n", url, message);
+    if (curl) {
+        CURLcode res;
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, message);
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK) {
+            return REQ_ERR;
+        }
+
+        curl_easy_cleanup(curl);
+    } else {
+        return INIT_ERR;
+    }
+    return OK;
 }
 
-static void send_put_request(char *url, char *message) {
+static int send_put_request(char *url, char *message) {
     printf("sending put request at url: %s and message: %s\n", url, message);
+    if (curl) {
+        CURLcode res;
+        headers = curl_slist_append(headers, "Content-Type: application/json");
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT");
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, message);
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK) {
+            return REQ_ERR;
+        }
+
+        curl_easy_cleanup(curl);
+    } else {
+        return INIT_ERR;
+    }
+    return OK;
 }
 
-static void send_delete_request(char *url, char *message) {
+static int send_delete_request(char *url, char *message) {
     printf("sending delete request at url: %s and message: %s\n", url, message);
+    if (curl) {
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK) {
+            return REQ_ERR;
+        }
+
+        curl_easy_cleanup(curl);
+    } else {
+        return INIT_ERR;
+    }
+    return OK;
 }
 
 
@@ -74,15 +140,18 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
                 break;
             }
             arguments->arg = arg;
+            break;
         case ARGP_KEY_END:
             if (arguments->url == NULL) {
                 printf("Missing required url, please provide to continue\n");
                 argp_usage (state);
+                break;
             }
             break;
         case ARGP_KEY_SUCCESS:
             if (arguments->get) {
-                send_get_request(arguments->url, arguments->arg);
+                printf("sending get request\n");
+                send_get_request(arguments->url);
             } else if (arguments->post) {
                 send_post_request(arguments->url, arguments->arg);
             } else if (arguments->put) {

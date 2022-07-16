bbab7b59c858ec0ab1ffbaff47c1f34a266013a7
jhilland <hillandjoseph@gmail.com>
2022-07-15 19:17:02 -0600

    adding hw zip and next hw for java server on aws

---
+++ code/hilland_final/hillandfinal.c
@@ -0,0 +1,158 @@
+// for standard gcc: make -f make-gcc
+// for arm: make -f make-arm
+#include <stdio.h>
+#include <argp.h>
+#include <stdbool.h>
+#include <curl/curl.h>
+#include <string.h>
+
+#define NO_ARG      0
+#define OK          0
+#define INIT_ERR    1
+#define REQ_ERR     2
+
+/*
+ * Hilland, Joseph ECE 531 Final Assignment
+ */
+
+const char *argp_program_version = "1.0.0.dev1";
+const char *argp_program_bug_address = "jhilland@unm.edu";
+static char args_doc[] = "-u http://localhost:8000 -o 'argument to pass'";
+static char doc[] = "Provide a url and conduct a get, post, delete or put request.";
+
+// arguments will be used for storing values from command line
+struct Arguments {
+    char *arg;  // for string argument
+    char *url;    
+    bool post;
+    bool get;
+    bool put;
+    bool delete;
+};
+
+// argp options required for output to user
+static struct argp_option options[] = {
+    {"url", 'u', "String", NO_ARG, "URL for HTTP Request, REQUIRED"},
+    {"post", 'o', NO_ARG, NO_ARG, "POST HTTP Request, requires VERB"},
+    {"get", 'g', NO_ARG, NO_ARG, "GET HTTP Request"},
+    {"put", 'p', NO_ARG, NO_ARG, "GET HTTP Request, requires VERB"},
+    {"delete", 'd', NO_ARG, NO_ARG, "GET HTTP Request, requires VERB"},
+    {NO_ARG}
+};
+
+static int send_http_requeset(char *url, char *message, char *type, bool verb) {
+    printf("sending %s request at url: %s\n", type, url);
+    CURL *curl = curl_easy_init();
+    if (curl) {
+        CURLcode res;
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, type);
+        if (verb) {
+            printf("sending message: %s\n", message);
+            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, message);
+        } else {
+            curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+        }
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK) {
+            return REQ_ERR;
+        }
+
+        curl_easy_cleanup(curl);
+    } else {
+        return INIT_ERR;
+    }
+    return OK;
+}
+
+int handle_requirement_error(char* message, struct argp_state *state) {
+    printf(message);
+    argp_usage(state);
+    return REQ_ERR;
+}
+
+// function to parse commandline options and arguments
+static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+    struct Arguments *arguments = state->input;
+
+    switch (key) {
+        case 'u':
+            arguments->url = arg;
+            break;
+        case 'o':
+            arguments->post = true;
+            break; 
+        case 'g':
+            arguments->get = true;
+            break;
+        case 'p':
+            arguments->put = true;
+            break;
+        case 'd':
+            arguments->delete = true;
+            break;
+        case ARGP_KEY_NO_ARGS:
+            // check if args are required based on request type, notify user
+            if (arguments->post == true || arguments->put == true || arguments->delete == true) {
+                return handle_requirement_error("You need to supply a VERB.\n", state);
+            }
+        case ARGP_KEY_ARG:
+            // if too many arguments are given, notify user
+            if (state->arg_num >= 1) {
+                printf("Too many arguments, use quotes around your extra argument.\n");
+                argp_usage(state);
+                return REQ_ERR;
+            }
+            arguments->arg = arg;
+            break;
+        case ARGP_KEY_END:
+            // if url is null or malformed, notify user
+            if (arguments->url == NULL) {
+                printf("Please provide a valid url.\n");
+                argp_usage(state);
+                return REQ_ERR;
+            } else if (arguments->get == false && arguments->post == false && arguments->put == false && arguments->delete == false) {
+                return handle_requirement_error("You must select http request type.\n", state);
+            }
+            break;
+        case ARGP_KEY_SUCCESS:
+            // perform request based on type, should this be limited to only one type allowed...
+            if (arguments->get) {
+                int err = send_http_requeset(arguments->url, NULL, "GET", false);
+                break;
+            } else if (arguments->post) {
+                int err = send_http_requeset(arguments->url, arguments->arg, "POST", true);
+                break;
+            } else if (arguments->put) {
+                int err = send_http_requeset(arguments->url, arguments->arg, "PUT", true);
+                break;
+            } else if (arguments->delete) {
+                int err = send_http_requeset(arguments->url, arguments->arg, "DELETE", true);
+                break;
+            }
+            break;
+        default:
+            return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+static struct argp argp = {options, parse_opt, args_doc, doc};
+
+int main(int argc, char **argv) {
+    struct Arguments arguments;
+
+    // default arguments, which could be done in struct
+    arguments.url = NULL;
+    arguments.arg = NULL;
+    arguments.post = false;
+    arguments.get = false;
+    arguments.put = false;
+    arguments.delete = false;
+
+    // parse the arguments
+    argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+    return 0;
+}
---
+++ code/hilland_hw4/sample_daemon.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <syslog.h>
+#include <time.h>
+#include <errno.h>
+#include <assert.h>
+
+#define OK              0
+#define ERR_SETSID      1
+#define SIGTERM         2
+#define SIGHUP          3
+#define ERR_FORK        4
+#define ERR_CHDIR       5
+#define ERR_WTF         9
+#define DAEMON_NAME     "Test_daemon"
+
+static void _signal_handler(const int signal) {
+    switch (signal) {
+        case SIGHUP:
+            break;
+        case SIGTERM:
+            syslog(LOG_INFO, "received SIGTERM, exiting.");
+            closelog();
+            exit(OK);
+            break;
+        default:
+            syslog(LOG_INFO, "received unhandled signal");
+            closelog();
+            exit(ERR_WTF);
+    }
+}
+
+void _get_time(void) {
+    while (1) {
+        time_t now;
+        time(&now);
+        syslog(LOG_INFO, "Time Now: %s", ctime(&now));
+        sleep(1);
+    }
+    syslog(LOG_INFO, "ending time...");
+}
+
+int main() {
+    openlog(DAEMON_NAME, LOG_PID | LOG_NDELAY | LOG_NOWAIT, LOG_DAEMON);
+
+    syslog(LOG_INFO, "Daemon sampled");
+
+    // fork will return twice
+    // it will return -1 if it failed
+    // it will return 0 in child proc if it succeeds
+    // returns PID in parent process if it succeeds
+    pid_t pid = fork();
+
+    // exit if fork fails
+    if (pid < 0) {
+        syslog(LOG_ERR, strerror(errno));
+        return ERR_FORK;
+    }
+
+    // check that parent fork returns pid of child, child
+    // fork will return success
+    if (pid > 0) {
+        return OK;
+    }
+
+
+    // create session, set process group leader
+    if (setsid() < -1) {
+        syslog(LOG_ERR, strerror(errno));
+        return ERR_SETSID;
+    }
+
+    // standard to close other output/input streams
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+    umask(S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+    // change to root directory for safety
+    if (chdir("/") < 0) {
+        syslog(LOG_ERR, strerror(errno));
+        return ERR_CHDIR;
+    }
+
+    signal(SIGTERM, _signal_handler);
+    signal(SIGHUP, _signal_handler);
+
+    _get_time();
+
+    return ERR_WTF;
+}

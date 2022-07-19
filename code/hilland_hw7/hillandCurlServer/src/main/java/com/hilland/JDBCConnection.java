ac1be9f7681f7a7ffc39a5c8dcd21ccfad86b107
jhilland <hillandjoseph@gmail.com>
2022-07-19 07:36:18 -0600

    password

--- code/hilland_hw7/hillandCurlServer/src/main/java/com/hilland/JDBCConnection.java
+++ code/hilland_hw7/hillandCurlServer/src/main/java/com/hilland/JDBCConnection.java
@@ -12,7 +12,7 @@ public class JDBCConnection {
 
     public JDBCConnection() {
         try (Connection conn = DriverManager.getConnection(
-                "jdbc:mysql://127.0.0.1:3306/test", "root", "password")) {
+                "jdbc:mysql://127.0.0.1:3306/test", "root", "Brady#2019")) {
 
             if (conn != null) {
                 System.out.println("Connected to the database!");

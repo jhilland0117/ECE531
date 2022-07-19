8b3c9bb7425317cc67411708ae2a2cdbfdb5137a
jhilland <hillandjoseph@gmail.com>
2022-07-19 08:28:15 -0600

    skeleton for other requests

--- code/hilland_hw7/hillandCurlServer/src/main/java/com/hilland/HillandCurlServer.java
+++ code/hilland_hw7/hillandCurlServer/src/main/java/com/hilland/HillandCurlServer.java
@@ -7,6 +7,7 @@ import fi.iki.elonen.NanoHTTPD.Response;
 import java.io.IOException;
 import java.util.List;
 import com.google.gson.Gson;
+import java.util.HashMap;
 
 import static fi.iki.elonen.NanoHTTPD.newFixedLengthResponse;
 
@@ -43,10 +44,26 @@ public class HillandCurlServer extends NanoHTTPD {
             Gson gson = new Gson();
             String jsonResp = gson.toJson(consoles);
             return newFixedLengthResponse(jsonResp);
-            // use for post
-            // String parameter = session.getParms().get("itemId");
-            //return newFixedLengthResponse("Requested itemId = " + parameter);
+        } else if (session.getMethod() == Method.POST) {
+            System.out.println("received a post");
+            try {
+                session.parseBody(new HashMap<>());
+                String requestBody = session.getQueryParameterString();
+                return newFixedLengthResponse("Request body = " + requestBody);
+            } catch (IOException | ResponseException e) {
+                return failedAttempt();
+            }
+        } else if (session.getMethod() == Method.PUT) {
+            System.out.println("received a put");
+        } else if (session.getMethod() == Method.DELETE) {
+            System.out.println("received a delete");
         }
+        
+        return failedAttempt();
+    }
+
+    private Response failedAttempt() {
+        // all attempts have failed
         return newFixedLengthResponse(Response.Status.NOT_FOUND, MIME_PLAINTEXT,
                 "The requested resource does not exist");
     }

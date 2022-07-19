9d9aa88e72e9ebc7c0df4744c3a215c5861955b1
jhilland <hillandjoseph@gmail.com>
2022-07-19 07:34:38 -0600

    create database class

--- code/hilland_hw7/hillandCurlServer/src/main/java/com/hilland/HillandCurlServer.java
+++ code/hilland_hw7/hillandCurlServer/src/main/java/com/hilland/HillandCurlServer.java
@@ -1,25 +1,29 @@
 package com.hilland;
 
-import java.io.IOException;
-import java.util.Map;
 import fi.iki.elonen.NanoHTTPD;
+import fi.iki.elonen.NanoHTTPD.IHTTPSession;
+import fi.iki.elonen.NanoHTTPD.Response;
+import static fi.iki.elonen.NanoHTTPD.newFixedLengthResponse;
+import java.io.IOException;
 
 /**
  * Base application class for setting up NanoHTTPD server.
- * 
+ *
  * @author jhilland
  */
-
 public class HillandCurlServer extends NanoHTTPD {
 
     public HillandCurlServer() throws IOException {
         super(8080);
         start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
-        System.out.println("\nRunning! Point your browsers to http://localhost:8080/ \n");
     }
 
     public static void main(String[] args) {
         try {
+            // connect to the database
+            JDBCConnection connection = new JDBCConnection();
+            
+            // create new instance of server
             new HillandCurlServer();
         } catch (IOException ioe) {
             System.err.println("Couldn't start server:\n" + ioe);
@@ -28,13 +32,12 @@ public class HillandCurlServer extends NanoHTTPD {
 
     @Override
     public Response serve(IHTTPSession session) {
-        String msg = "<html><body><h1>Hello server</h1>\n";
-        Map<String, String> parms = session.getParms();
-        if (parms.get("username") == null) {
-            msg += "<form action='?' method='get'>\n  <p>Your name: <input type='text' name='username'></p>\n" + "</form>\n";
-        } else {
-            msg += "<p>Hello, " + parms.get("username") + "!</p>";
+        if (session.getMethod() == Method.GET) {
+            String parameter = session.getParms().get("itemId");
+            System.out.println("HERE IT IS: " + parameter);
+            return newFixedLengthResponse("Requested itemId = " + parameter);
         }
-        return newFixedLengthResponse(msg + "</body></html>\n");
+        return newFixedLengthResponse(Response.Status.NOT_FOUND, MIME_PLAINTEXT,
+                "The requested resource does not exist");
     }
 }
---
+++ code/hilland_hw7/hillandCurlServer/src/main/java/com/hilland/JDBCConnection.java
@@ -0,0 +1,30 @@
+package com.hilland;
+
+/**
+ *
+ * @author jhilland
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCConnection {
+
+    public JDBCConnection() {
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://127.0.0.1:3306/test", "root", "password")) {
+
+            if (conn != null) {
+                System.out.println("Connected to the database!");
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
